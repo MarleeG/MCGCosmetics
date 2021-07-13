f18e0718fd7db1e44d7a9b088e669bf31316dd41
Marlee Gerard <marleegerard@gmail.com>
2021-07-12 21:06:49 -0400

    simplied the routes and controllers

---
+++ controllers/data.js
@@ -0,0 +1,14 @@
+const apiBasicInfo = [
+  { id: 0, name: "Blush", api_product_type_value: "" },
+  { id: 1, name: "Bronzer", api_product_type_value: "bronzer" },
+  { id: 2, name: "Eyebrow", api_product_type_value: "eyebrow" },
+  { id: 3, name: "Eyeliner", api_product_type_value: "eyeliner" },
+  { id: 4, name: "Eyeshadow", api_product_type_value: "eyeshadow" },
+  { id: 5, name: "Foundation", api_product_type_value: "foundation" },
+  { id: 6, name: "Lip liner", api_product_type_value: "lip_liner" },
+  { id: 7, name: "Lipstick", api_product_type_value: "lipstick" },
+  { id: 8, name: "Mascara", api_product_type_value: "mascara" },
+  { id: 9, name: "Nail Polish", api_product_type_value: "nail_polish" }
+];
+
+module.exports = apiBasicInfo;
--- routes/product.js
+++ routes/product.js
@@ -1,29 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getAllBlushProducts,
-  getAllBronzerProducts,
-  getAllEyebrowProducts,
-  getAllEyelinerProducts,
-  getAllEyeshadowProducts,
-  getAllFoundationProducts,
-  getAllLiplinerProducts,
-  getAllLipstickProducts,
-  getAllNailPolishProducts,
-  getAllMascaraProducts
-} = require("../controllers/product");
+const { getAllProductsByType } = require("../controllers/product");
 
-router
-  .get("/blush", getAllBlushProducts)
-  .get("/bronzer", getAllBronzerProducts)
-  .get("/eyebrow", getAllEyebrowProducts)
-  .get("/eyeliner", getAllEyelinerProducts)
-  .get("/eyeshadow", getAllEyeshadowProducts)
-  .get("/foundation", getAllFoundationProducts)
-  .get("/lip_liner", getAllLiplinerProducts)
-  .get("/lipstick", getAllLipstickProducts)
-  .get("/mascara", getAllMascaraProducts)
-  .get("/nail_polish", getAllNailPolishProducts);
+router.get("/:product_type", getAllProductsByType);
 
 module.exports = router;
